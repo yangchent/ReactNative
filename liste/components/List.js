e6e83ebf5045384c46f49db461d8d86af10991c6
yangchen <gyachungriom@gmail.com>
2021-08-25 15:15:05 +0200

    liste completed

--- liste/components/List.js
+++ liste/components/List.js
@@ -3,12 +3,12 @@ import { StyleSheet, FlatList, View, Text, Image, TouchableOpacity } from 'react
 
 export default function List() {
     const [countries, setCountries]= useState([]);
-    // const [weather, setWeather]= useState([]);
-    // const [pressed, setPressed] = useState(true);
+    const [selectedCapital, setSelectedCapital]= useState([]);
+    const [weather, setWeather]= useState([]);
+    const [pressed, setPressed] = useState(false);
 
     useEffect(() => {
         getContries();
-        // getWeather();
         }, []);
         
         const getContries = (() => {
@@ -19,28 +19,34 @@ export default function List() {
             });
             })
 
-            // const getWeather =(({item}) => {
-            //      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${item.capital}&appid=6fe34ca84af8fb9fb507b71a4e864964`)
-            //      .then((res) => res.json()) 
-            //      .then((res) => { 
-            //       setWeather(res);
-            //     });
-            // })
+            const getWeather =((capital) => {
+                 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=6fe34ca84af8fb9fb507b71a4e864964`)
+                 .then((res) => res.json()) 
+                 .then((res) => { 
+                  setWeather(res.main.temp);
+                });
+            })
 
-            // const pressHandle = ()=>{
-            //     return <View>
-            //         <Text style={styles.Text}>Capital city: {item.capital}</Text>
-                    
-            //     </View>
-            // }
+            const pressHandle = (capital)=>{
+                    setPressed(true);
+                    getWeather(capital)
+            }
           const renderCountries = ({ item }) => {
+              setSelectedCapital(item.capital);
             return <View>
                 <Text style={styles.Text}>Country Name: {item.name}</Text>
-                <TouchableOpacity onPress={()=>console.log('pressed')}>
+                <TouchableOpacity onPress={()=>pressHandle(item.capital)}>
                 <Image source={{uri:item.flag}} style={styles.Image}/>
                 </TouchableOpacity>
+                {pressed? 
+                <View>
+                    <Text style={styles.Text}>Capital city: {item.capital}</Text>
+                    <Text>{weather}</Text>
+                </View>
+                : null }
             </View>
            }
+           console.log(selectedCapital);
     return(
         <View>
             <FlatList
